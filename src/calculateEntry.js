b2cac7685baba16e09000782ad5bda839746a735
Nina Palma <ninapalmadev@gmail.com>
2023-04-11 17:23:03 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
+const { adult, child, senior } = data.prices;
+
+const countEntrants = (entrants) => (entrants.reduce((acc, person) => {
+  if (person.age < 18) {
+    acc.child += 1;
+    return acc;
+  }
+  if (person.age >= 50) {
+    acc.senior += 1;
+    return acc;
+  }
+  acc.adult += 1;
+  return acc;
+}, { adult: 0, child: 0, senior: 0 }));
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || entrants.length === 0) return 0;
+  const count = countEntrants(entrants);
+  const result = (count.adult * adult) + (count.child * child) + (count.senior * senior);
+  return result.toFixed([2]);
 };
 
 module.exports = { calculateEntry, countEntrants };
