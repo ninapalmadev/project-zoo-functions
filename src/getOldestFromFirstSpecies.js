83ce30e3f528839a29216905b94e85e21368102f
Nina Palma <ninapalmadev@gmail.com>
2023-04-11 19:28:19 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const employeeId = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const firstSpecie = species.find((specieId) => specieId.id === employeeId);
+  const olderAge = firstSpecie.residents.reduce((acc, currAge) => {
+    if (acc > currAge.age) {
+      return acc;
+    }
+    return currAge.age;
+  }, 0);
+  const animal = firstSpecie.residents.find((animalFind) => animalFind.age === olderAge);
+  return [animal.name, animal.sex, olderAge];
 };
 
 module.exports = getOldestFromFirstSpecies;
