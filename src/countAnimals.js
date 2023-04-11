6e85e2a335eea996ccb1de17edc33792a2eed054
Nina Palma <ninapalmadev@gmail.com>
2023-04-11 16:51:28 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce(((acc, specie) => {
+      acc[`${specie.name}`] = specie.residents.length;
+      return acc;
+    }), {});
+  }
+  const { species: animalSpecie, sex: animalSex } = animal;
+
+  const specieSex = species.find((specie) => specie.name.includes(animalSpecie))
+    .residents.filter((resident) => (animalSex ? resident.sex === animalSex : resident)).length;
+  return specieSex;
 };
 
 module.exports = countAnimals;
+
+// console.log(countAnimals());
+// console.log(countAnimals({ species: 'bears', sex: 'female' }));
+// console.log(countAnimals({ species: 'giraffes' }));
