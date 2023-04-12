b92b3a9351dd9bf134c22175c7fce9119c4e9520
Nina Palma <ninapalmadev@gmail.com>
2023-04-12 17:33:43 -0300

    requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
-};
+const { species } = data;
+
+const getSpecie = (residents, sex) =>
+  residents.filter((animal) => (sex ? animal.sex === sex : true))
+    .map((animal) => animal.name);
+
+const getNames = (sorted, sex) => species.reduce((acc, animal) => ({
+  ...acc,
+  [animal.location]:
+    [...(acc[animal.location] || []),
+      {
+        [animal.name]:
+        sorted ? getSpecie(animal.residents, sex).sort() : getSpecie(animal.residents, sex),
+      },
+    ],
+}), {});
+
+const getNamesUnsorted = () => species.reduce((acc, animal) => ({
+  ...acc,
+  [animal.location]:
+    [...(acc[animal.location] || []),
+      animal.name,
+    ],
+}), {});
+
+const getAnimalMap = ({ includeNames, sorted, sex } = {}) =>
+  (includeNames ? getNames(sorted, sex) : getNamesUnsorted());
 
 module.exports = getAnimalMap;
